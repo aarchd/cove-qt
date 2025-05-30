7d528b1b35f1024e64a80df132250bc7e81c3196
Deepak Meena <notwho53@gmail.com>
2025-05-30 10:02:29 +0530

    helper/battery
    Signed-off-by: Deepak Meena <notwho53@gmail.com>

---
+++ src/helper/battery.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <QObject>
+#include <libupower-glib/upower.h>
+#include <glib-2.0/glib.h>
+
+class Battery : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(int percentage READ percentage NOTIFY percentageChanged)
+    Q_PROPERTY(int state READ state NOTIFY stateChanged)
+
+public:
+    explicit Battery(QObject *parent = nullptr);
+    ~Battery();
+
+    int percentage() const;
+    int state() const;
+
+signals:
+    void percentageChanged();
+    void stateChanged();
+
+private:
+    static void onDeviceChanged(UpDevice *device, GParamSpec *pspec, gpointer user_data);
+
+    UpClient *m_client = nullptr;
+    UpDevice *m_device = nullptr;
+
+    int m_percentage = 0;
+    int m_state = 0;
+};
